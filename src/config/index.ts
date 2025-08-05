247205d7774a2263e8dccd799a2f45ea861a3cb6
lambert.li <l156486648@163.com>
2025-08-05 14:06:29 +0800

    feat: 添加 app  全局静态配置

---
+++ src/config/index.ts
@@ -0,0 +1,20 @@
+import type { AppInfo } from '@/types/app'
+export const API_KEY = 'app-8LRYB7Gz1nyEQHW22p5u3nMv'
+export const APP_ID = 'a794acb6-ee01-4d11-8284-83994fb41fea'
+export const API_URL = 'http://10.21.77.20:30001/v1'
+export const APP_INFO: AppInfo = {
+  title: 'Chat APP',
+  description: '',
+  copyright: '',
+  privacy_policy: '',
+  default_language: 'zh-Hans',
+}
+
+export const isShowPrompt = false
+export const promptTemplate = 'I want you to act as a javascript console.'
+
+export const API_PREFIX = '/api'
+
+export const LOCALE_COOKIE_NAME = 'locale'
+
+export const DEFAULT_VALUE_MAX_LEN = 48
