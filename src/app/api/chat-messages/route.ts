6fc8e2c8aef8518fa9c92bbd502c0080bf86b5cb
lambert.li <l156486648@163.com>
2025-08-06 11:29:34 +0800

    feat: 解决 sse 推送网络接口不展示 EventStream 消息详情问题

--- src/app/api/chat-messages/route.ts
+++ src/app/api/chat-messages/route.ts
@@ -11,6 +11,15 @@ export async function POST(request: NextRequest) {
     response_mode: responseMode,
   } = body
   const { user } = getInfo(request)
+  const isStreaming =  responseMode === "streaming"
   const res = await client.createChatMessage(inputs, query, user, responseMode, conversationId, files)
+  if(isStreaming) {
+    return new Response(res.data, {
+      headers: {
+        ...res.headers,
+        'Content-Type': 'text/event-stream',
+      },
+    })
+  } else 
   return new Response(res.data as any)
 }
